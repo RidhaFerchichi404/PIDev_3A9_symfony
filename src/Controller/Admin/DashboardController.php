389a8323704daa22024bc429108210d435066d0e
RidhaFerchichi404 <ridha.ferchichi2222@hotmail.com>
2025-04-30 18:51:44 +0000

    add new google bundle

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Repository\PostRepository;
 use App\Repository\CommentRepository;
+use CMEN\GoogleChartsBundle\GoogleCharts\Charts\PieChart;
+use CMEN\GoogleChartsBundle\GoogleCharts\Charts\BarChart;
+use CMEN\GoogleChartsBundle\GoogleCharts\Charts\Material\ColumnChart;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -27,12 +30,95 @@ class DashboardController extends AbstractController
         // Get recent comments
         $recentComments = $commentRepository->findBy([], ['date' => 'DESC'], 5);
 
+        // Create pie chart for post types
+        $pieChart = new PieChart();
+        $pieChartData = [['Type', 'Count']];
+        
+        // Only add data if we have post types
+        if (count($postsByType) > 0) {
+            foreach ($postsByType as $type) {
+                $pieChartData[] = [$type['type'], (int)$type['count']];
+            }
+        } else {
+            // Add dummy data if no post types exist
+            $pieChartData[] = ['No posts', 1];
+        }
+        
+        $pieChart->getData()->setArrayToDataTable($pieChartData);
+        
+        // Configure pie chart
+        $pieChart->getOptions()->setTitle('Posts by Type');
+        $pieChart->getOptions()->setHeight(400);
+        $pieChart->getOptions()->setWidth(600);
+        $pieChart->getOptions()->setIs3D(true);
+        $pieChart->getOptions()->getTitleTextStyle()->setBold(true);
+        $pieChart->getOptions()->getTitleTextStyle()->setColor('#009900');
+        $pieChart->getOptions()->getTitleTextStyle()->setItalic(true);
+        $pieChart->getOptions()->getTitleTextStyle()->setFontName('Arial');
+        $pieChart->getOptions()->getTitleTextStyle()->setFontSize(20);
+
+        // Create bar chart for comments by post (top 5 most commented)
+        $barChart = new BarChart();
+        $mostCommentedPosts = $postRepository->findMostCommented(1, 5);
+        $barChartData = [['Post', 'Comments']];
+        
+        // Only add data if we have commented posts
+        if (count($mostCommentedPosts) > 0) {
+            foreach ($mostCommentedPosts as $post) {
+                $commentCount = count($post->getComments());
+                $postTitle = substr($post->getDescription(), 0, 30) . '...';
+                $barChartData[] = [$postTitle, $commentCount];
+            }
+        } else {
+            // Add dummy data if no commented posts
+            $barChartData[] = ['No comments', 0];
+        }
+        
+        $barChart->getData()->setArrayToDataTable($barChartData);
+        
+        // Configure bar chart
+        $barChart->getOptions()->setTitle('Most Commented Posts');
+        $barChart->getOptions()->getHAxis()->setTitle('Comments');
+        $barChart->getOptions()->getVAxis()->setTitle('Posts');
+        $barChart->getOptions()->setWidth(600);
+        $barChart->getOptions()->setHeight(400);
+        $barChart->getOptions()->getBar()->setGroupWidth('75%');
+        $barChart->getOptions()->getLegend()->setPosition('none');
+
+        // Create a column chart for blog statistics
+        $statsChart = new ColumnChart();
+        $statsChartData = [
+            ['Metric', 'Count', ['role' => 'style']],
+            ['Posts', $totalPosts, '#4285F4'],
+            ['Comments', $totalComments, '#34A853']
+        ];
+        
+        $statsChart->getData()->setArrayToDataTable($statsChartData);
+        
+        // Configure column chart
+        $statsChart->getOptions()
+            ->setTitle('Blog Statistics')
+            ->setHeight(300)
+            ->setWidth(500)
+            ->setColors(['#4285F4', '#34A853'])
+            ->setFontName('Arial')
+            ->getLegend()->setPosition('none');
+
+        // Configure axes
+        $statsChart->getOptions()->getHAxis()->setTitle('Metric');
+        $statsChart->getOptions()->getHAxis()->getTitleTextStyle()->setBold(true);
+        $statsChart->getOptions()->getVAxis()->setTitle('Count');
+        $statsChart->getOptions()->getVAxis()->getTitleTextStyle()->setBold(true);
+
         return $this->render('admin/dashboard/index.html.twig', [
             'totalPosts' => $totalPosts,
             'totalComments' => $totalComments,
             'postsByType' => $postsByType,
             'recentPosts' => $recentPosts,
             'recentComments' => $recentComments,
+            'pieChart' => $pieChart,
+            'barChart' => $barChart,
+            'statsChart' => $statsChart,
         ]);
     }
 } 
